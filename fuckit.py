bc06fe48acf7222c0147b2610937b94555b79dc3
Matthew Hooker <mwhooker@gmail.com>
2013-09-13 18:58:34 -0700

    sort dat.

--- fuckit.py
+++ fuckit.py
@@ -34,14 +34,17 @@ def cb(arg, dirname, filenames):
     for f in filenames:
         parsed[f] = parse(os.path.join(dirname, f))
 
-    with open(os.path.join(outdir, 'section.md'), 'w+') as outf:
-        for fname, sections in parsed.items():
-            print "[generating] %s" % fname
-            for section in sections:
-                secnum = section[0].split()[0]
-                outf.write("## [%s](#%s)\n" % (secnum, secnum))
-                outf.write(" ".join(section))
-                outf.write("\n")
+    ssect = []
+    for fname, sections in parsed.items():
+        print "[generating] %s" % fname
+        for section in sections:
+            secnum = section[0].split()[0]
+            ssect.append((secnum, section))
 
+    with open(os.path.join(outdir, 'section.md'), 'w+') as outf:
+        for secnum, section in sorted(ssect):
+            outf.write("## [%s](#%s)\n" % (secnum, secnum))
+            outf.write(" ".join(section))
+            outf.write("\n")
 
 os.path.walk(indir, cb, None)
